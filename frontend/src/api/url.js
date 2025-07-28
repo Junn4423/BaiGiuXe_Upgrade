87859163a6e8638a673d1ce7b334adb143c503ff
Junn4423 <chungluong4423@gmail.com>
2025-07-28 15:03:57 +0700

    hello youtube

--- frontend/src/api/url.js
+++ frontend/src/api/url.js
@@ -1,4 +1,5 @@
-export const api_BienSo = "http://192.168.1.20:8000/detect"
-export const url_api = "http://192.168.1.94/parkinglot/services.sof.vn/index.php"
-export const url_login_api = "http://192.168.1.94/parkinglot/login.sof.vn/index.php"
-export const url_api_chamcong = "http://192.168.1.20:5005"
+export const api_BienSo = "http://192.168.0.216:8000/detect"
+export const url_api = "http://192.168.0.216/services/parkinglot/services.sof.vn/index.php"
+export const url_login_api = "http://192.168.0.216/services/parkinglot/login.sof.vn/index.php"
+export const url_api_chamcong = "http://192.168.0.216:5005"
+
